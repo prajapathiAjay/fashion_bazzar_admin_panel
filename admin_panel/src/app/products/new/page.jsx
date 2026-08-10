3e4bdd73c7bb095a33dfdc37158b099a04a532dd
Ajay <ajayworking6309@gmail.com>
2026-08-10 17:24:48 +0000

    started with next js folder structure done

---
+++ admin_panel/src/app/products/new/page.jsx
@@ -0,0 +1,4 @@
+export default function NewProduct() {
+return(<>Add new product page</>)
+
+}
---
+++ admin_panel/src/app/products/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Products(){
+
+    return (
+    <><div>Products page
+    
+
+    </div>
+     <Link href="/products/new">Add New Product</Link></>)
+}
